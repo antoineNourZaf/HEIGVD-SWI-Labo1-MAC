47487d959f2db5ef281a637925556a4aa901e2ad
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2019-03-21 16:40:08 +0100

    updated question 2 script

--- sniffer2.py
+++ sniffer2.py
@@ -5,16 +5,22 @@ import requests
 from scapy.layers.dot11 import Dot11ProbeReq
 from scapy.sendrecv import sniff
 
-clients = []
+clients = [][]
 
 # Function to execute when a packet was found
 def stationFound(packet):
 
     # Looking for probe requests which is given by stations
     if (packet.haslayer(Dot11ProbeReq)):
+
+        # If it's the first time we see the device, it will be put in the array
+        if (packet.addr2 not in clients):
+            clients.append(packet.addr2)
+        else :
+            clients[clients.index(packet.addr2)].append(packet.info)
         # We get informations from API to get manufacturer
         r = requests.get("http://macvendors.co/api/vendorname/" + packet.addr2 + "/pipe")
-        print(packet.addr2 + " (" + r.content + ") " + packet.info)
+        print(packet.addr2 + " (" + r.content + ") " + clients[clients.index(packet.addr2)])
 
 if __name__ == '__main__':
 
